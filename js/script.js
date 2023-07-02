f40e2754f8303e67e4da815ee5117c62e6d1c551
Nozim Yusupov <nozimy@yandex.ru>
2023-07-02 21:26:06 +0500

    Second Edition

--- js/script.js
+++ js/script.js
@@ -2,8 +2,23 @@ const rock = 'камень';
 const paper = 'бумага';
 const scissors = 'ножницы';
 
+const container = document.querySelector('.container');
+const buttons = document.querySelectorAll('.btn');
+//const results = document.querySelector('.results');
+const results = document.createElement('div');
+const wins = document.createElement('div');
+
+results.classList.add('results');
+
+container.appendChild(results);
+
+wins.classList.add('wins');
+wins.style.cssText = 'text-align: center; color: red; font-size: 22px; font-weight: bold; font-style: italic;';
+
+
 let playerScore = 0;
 let computerScore = 0;
+
 
 function getRandomNumber(min, max) {
   let rand = Math.random() * (max - min) + min;
@@ -40,14 +55,18 @@ function playRound(playerSelection, computerSelection) {
     
 }
 
-function playerPlay() {
+
+
+function playerPlayOld() {
   let result = prompt("Игра камень ножницы бумага. Что вводите.");
   return result;
 }
-function game() {
+
+
+function gameOld() {
 
   for (let i = 0; i < 5; i++) {
-    let playerSelection = playerPlay();
+    let playerSelection = playerPlayOld();
     let computerSelection = computerPlay();
     let roundResult = playRound(playerSelection, computerSelection);
     alert(roundResult);
@@ -57,10 +76,43 @@ function game() {
       computerScore += 1;
     }
   }
+  
+  alert(showResult);
+}
+
+function showResult() {
   let result = "Общий счёт: Человек - " + playerScore + " компьютер - " + computerScore; 
   let wins = playerScore > computerScore ? "Выиграл человек!" : 
              playerScore < computerScore ? "Выиграл компьютер!" : "Ничья!";
-  alert(result + "\n" + wins);
+  return result + "\n" + wins;
+
 }
 
+function game() {
+  let playerSelection = null;
+  buttons.forEach(btn => {     
+      btn.addEventListener('click', e => {
+        let computerSelection = computerPlay();
+ 
+        playerSelection = e.target.innerText;
+        let roundResult = playRound(playerSelection, computerSelection);
+       
+        if (roundResult.includes('выиграли')) {
+          playerScore += 1;       
+        } else if (roundResult.includes('проиграли')) {
+          computerScore += 1;
+        }
+        results.innerText = roundResult;
+        if (playerScore === 5 || computerScore === 5) {
+         wins.innerText = showResult();
+         results.appendChild(wins);
+         alert('Game Over');
+         computerScore = 0;
+         playerScore = 0;
+        }
+      });
+     
+  });
+
+}
 game();
